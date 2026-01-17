2970314ca1740502ec5e4f9d2d659bbc55a2af01
C-Ezra-M <wmarzec06.wm@gmail.com>
2026-01-17 22:04:44 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(
     ({ command, mode, isSsrBuild, isPreview }) => {
+        const commonConfig = defineConfig({
+            build: {
+                rollupOptions: {
+                    input: {
+                        main: resolve(__dirname, 'index.html'),
+                        help: resolve(__dirname, 'help.html'),
+                    },
+                },
+            }
+        });
         if (command === 'build') {
             return {
+                ...commonConfig,
                 base: "/algoboard/",
             }
         } else {
-            return {}
+            return commonConfig
         }
     }
 )
